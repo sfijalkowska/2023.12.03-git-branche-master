44f65d317acb4d4bd250f67f61f82c03ab294a1c
Sylwia Fijałkowska <fijalkowska.sylwia@outlook.com>
2024-01-21 10:01:46 +0100

    Initial commit

---
+++ 2023.12.03-git-branche-master/src/pl/camp/it/git/App.java
@@ -0,0 +1,21 @@
+package pl.camp.it.git;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("pierwsza linia !!");
+
+        System.out.println("Kolejna linia !");
+
+        System.out.println("Nowa linijka w masterze !!");
+
+        System.out.println("Nowe rzeczy !!");
+
+        System.out.println("Kolejna nowa rzecz !!");
+
+        System.out.println("Juz ostatnia rzecz !!");
+
+        System.out.println("Nowa linijka w masterze !!");
+
+        System.out.println("Linijka w feature branchu !!");
+    }
+}
